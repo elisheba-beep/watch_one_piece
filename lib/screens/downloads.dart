c6f3b3fb9b551c97ff2102c0334167b00a681b70
elisheba-beep <ekunife35@gmail.com>
2024-08-22 00:05:33 +0100

    start downloads screen ui

--- lib/screens/downloads.dart
+++ lib/screens/downloads.dart
@@ -10,9 +10,113 @@ class Downloads extends StatefulWidget {
 class _DownloadsState extends State<Downloads> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("downloads"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Text(
+            "Downloads",
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "43 videos  30 hours  3.9 GB",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                FilledButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Edit",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(25.0),
+                      child: Image.asset(
+                          height: 150,
+                          width: 200,
+                          'assets/images/carousel1.jpeg'),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Wano Arc",
+                            style: TextStyle(
+                              color: Theme.of(context).secondaryHeaderColor,
+                              fontSize: 22,
+                            ),
+                          ),
+                          Text(
+                            "110 episodes  5.5 GB",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      ">",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                padding: const EdgeInsets.all(15.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Download more videos",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
